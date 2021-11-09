a92345736222520862c00b23eab65cf964a1a50a
puneet56 <puneetdhiman56@gmail.com>
2021-11-09 22:35:39 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import {BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
 
 function App() {
-  return (
-    <BrowserRouter>
-    <div className="pt-20">
-      <Navbar />
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/about" component={About} exact />
-      </Switch>
-    </div>
-    </BrowserRouter>
-  )
+	return <div>Hello</div>;
 }
 
-export default App
+export default App;
